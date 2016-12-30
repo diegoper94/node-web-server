f67f3a84c1b8510f32534ce53a216432c96c3fb3
Diego Perdomo <diegoperdomo_94@hotmail.com>
2016-12-30 10:18:51 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const hbs = require ('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method}, ${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log", log);
+  next();
+});
+
+//app.use((req,res, next) => {
+//  res.render("maintenance.hbs");
+//});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Some Text",
+    welcomeMessage: "Welcome to the page"
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: "Unable to connect to server"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is ready at port 3000");
+});
